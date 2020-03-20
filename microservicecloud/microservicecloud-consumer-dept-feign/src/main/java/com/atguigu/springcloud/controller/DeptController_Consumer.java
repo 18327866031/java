9a10edcc064bc44e73ac74bf538668c589bd76bd
LAPTOP-Q201CIPC\huhua <aaronhh@sina.cn>
2020-03-20 21:29:57 +0800

    springcloud 入门项目

---
+++ microservicecloud/microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -0,0 +1,50 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DeptController_Consumer
+{
+    @Autowired
+    private DeptClientService deptClientService = null;
+
+
+
+    @RequestMapping(value="/consumer/dept/add")
+    public boolean add(@RequestBody Dept dept)
+    {
+        return deptClientService.add(dept);
+    }
+
+    @RequestMapping(value="/consumer/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id)
+    {
+        return deptClientService.get(id);
+    }
+
+
+    @RequestMapping(value="/consumer/dept/list")
+    public List<Dept> list()
+    {
+        System.out.println("*****************************************************");
+        return deptClientService.list();
+    }
+
+    //测试@EnableDiscoveryClient,消费端可以调用服务发现
+    @RequestMapping(value="/consumer/dept/discovery")
+    public Object discovery()
+    {
+        return deptClientService.discovery();
+    }
+}
+
+
+
